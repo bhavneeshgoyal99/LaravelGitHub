25bbd44b2c5e3466ced25f0ac3991eaa790fcb90
Bhavneesh Goyal <33285991+bhavneeshgoyal99@users.noreply.github.com>
2025-03-18 16:04:19 +0530

    Update LogoutCommand.php

--- src/Commands/LogoutCommand.php
+++ src/Commands/LogoutCommand.php
@@ -11,6 +11,25 @@ class LogoutCommand extends Command
 
     public function handle()
     {
-        $this->info('You have logged out of GitHub. Remember to delete the GITHUB_TOKEN from .env.');
+        $envFile = base_path('.env');
+        
+        if (!file_exists($envFile)) {
+            $this->error('.env file not found.');
+            return;
+        }
+    
+        // Remove GitHub credentials from the .env file
+        $envContent = file_get_contents($envFile);
+        $envContent = preg_replace('/^GITHUB_USERNAME=.*$/m', '', $envContent);
+        $envContent = preg_replace('/^GITHUB_TOKEN=.*$/m', '', $envContent);
+    
+        file_put_contents($envFile, $envContent);
+    
+        // Clear the Laravel config cache
+        \Artisan::call('config:clear');
+        \Artisan::call('cache:clear');
+    
+        $this->info('You have logged out of GitHub. Credentials have been removed from .env and cache.');
     }
+    
 }
